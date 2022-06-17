8b4d392afe4653a8b0a3227132de52d2cbd98cb3
pocari <caffelattenonsugar@gmail.com>
2022-06-18 03:46:45 +0900

    tailwind使えるように

--- app/javascript/component/HelloWorld/index.tsx
+++ app/javascript/component/HelloWorld/index.tsx
@@ -9,6 +9,7 @@ const HelloWorld: FC<Props> = (props: Props) => {
   console.log(`props: ${props}`);
   return (
     <div>
+      <button className="bg-indigo-700 font-semibold text-white py-2 px-4 rounded">button</button>
       <h3>react component</h3>
       { props.arg1 }, { props.arg2 }, hello, world
     </div>
